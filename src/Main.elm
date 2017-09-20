98cc0073a0667e4efca85e8aa88ae307e9713f68
Sindre Ilebekk Johansen <sindre@iterate.no>
2017-09-20 15:11:29 +0200

    Send/Retrieve counts

--- src/Main.elm
+++ src/Main.elm
@@ -7,6 +7,8 @@ import Dict exposing (Dict)
 import Html exposing (Html, main_, header, h1, text, div, button, input, form)
 import Html.Attributes exposing (class, value)
 import Html.Events exposing (onClick, onInput, onSubmit)
+import Http
+import Json.Decode as Decode
 
 
 type alias Model =
@@ -15,16 +17,27 @@ type alias Model =
     }
 
 
-init : Model
+getThings : Http.Request (Dict String Int)
+getThings =
+    Http.get "http://localhost:5000/counters" (Decode.dict Decode.int)
+
+
+incThing : String -> Http.Request (Dict String Int)
+incThing thing =
+    Http.post ("http://localhost:5000/counters/" ++ thing ++ "/increment") Http.emptyBody (Decode.dict Decode.int)
+
+
+decThing : String -> Http.Request (Dict String Int)
+decThing thing =
+    Http.post ("http://localhost:5000/counters/" ++ thing ++ "/decrement") Http.emptyBody (Decode.dict Decode.int)
+
+
+init : ( Model, Cmd Msg )
 init =
-    { counters =
-        Dict.fromList
-            [ ( "Knapper", 0 )
-            , ( "Glansbilder", 0 )
-            , ( "Kroner", 0 )
-            ]
+    { counters = Dict.empty
     , newThing = ""
     }
+        ! [ Http.send ThingsLoaded getThings ]
 
 
 
@@ -38,28 +51,37 @@ type Msg
     | DecrementAll
     | AddThing
     | NewThingInput String
+    | ThingsLoaded (Result Http.Error (Dict String Int))
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Increment thing ->
             { model | counters = Dict.update thing (Maybe.map (\c -> c + 1)) model.counters }
+                ! [ Http.send ThingsLoaded (incThing thing) ]
 
         Decrement thing ->
             { model | counters = Dict.update thing (Maybe.map (\c -> c - 1)) model.counters }
+                ! [ Http.send ThingsLoaded (decThing thing) ]
 
         IncrementAll ->
-            { model | counters = Dict.map (\_ n -> n + 1) model.counters }
+            { model | counters = Dict.map (\_ n -> n + 1) model.counters } ! []
 
         DecrementAll ->
-            { model | counters = Dict.map (\_ n -> n - 1) model.counters }
+            { model | counters = Dict.map (\_ n -> n - 1) model.counters } ! []
 
         AddThing ->
-            { model | counters = Dict.insert model.newThing 0 model.counters, newThing = "" }
+            { model | counters = Dict.insert model.newThing 0 model.counters, newThing = "" } ! []
 
         NewThingInput newThing ->
-            { model | newThing = newThing }
+            { model | newThing = newThing } ! []
+
+        ThingsLoaded (Ok counters) ->
+            { model | counters = counters } ! []
+
+        ThingsLoaded (Err err) ->
+            Debug.crash ("Oooops: " ++ (toString err))
 
 
 
@@ -97,8 +119,9 @@ viewCounter ( thing, count ) =
 
 main : Program Never Model Msg
 main =
-    Html.beginnerProgram
-        { model = init
+    Html.program
+        { init = init
         , view = view
         , update = update
+        , subscriptions = \_ -> Sub.none
         }
