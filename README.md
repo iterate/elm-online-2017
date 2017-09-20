ea672fe7e87abf81afbe1ec39807c281f1060c57
Sindre Ilebekk Johansen <sindre@iterate.no>
2017-09-20 12:39:22 +0200

    Hello World

---
+++ README.md
@@ -0,0 +1,7 @@
+# miniwoop-elm
+Example elm app
+
+```
+npm install -g elm elm-live
+elm-live --output=elm.js src/Main.elm --open --debug
+```
---
+++ src/Main.elm
@@ -0,0 +1,52 @@
+-- Model
+
+
+module Main exposing (..)
+
+import Html exposing (Html, main_, header, h1, text)
+
+
+type alias Model =
+    Int
+
+
+init : Model
+init =
+    0
+
+
+
+-- Update
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+update : Msg -> Model -> Model
+update msg model =
+    Debug.crash "TODO"
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view model =
+    main_ []
+        [ h1 [] [ text "Twish" ] ]
+
+
+
+-- Main
+
+
+main : Program Never Model Msg
+main =
+    Html.beginnerProgram
+        { model = init
+        , view = view
+        , update = update
+        }
