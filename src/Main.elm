3edbbac000026e46444c984490c05f061fd207e9
Sindre Johansen <sindre@iterate.no>
2017-09-21 18:41:04 +0200

    stuff

--- src/Main.elm
+++ src/Main.elm
@@ -2,3 +2,21 @@ module Main exposing (..)
 
 import Html exposing (Html, main_, header, h1, text)
 
+type alias Model =
+    Int
+
+-- Update
+
+type Msg
+    = Increment
+    | Decrement
+
+update : Msg -> Model -> Model
+update msg model =
+    Debug.crash "TODO"
+
+-- View
+
+view : Model -> Html Msg
+view model
+    main_ [] []
