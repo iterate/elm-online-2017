fce27ed45b1a2d01a2a7abf6bc2b52e3f5416505
Sindre Johansen <sindre@iterate.no>
2017-09-21 18:54:35 +0200

    stuff

--- src/Main.elm
+++ src/Main.elm
@@ -1,34 +1,47 @@
 module Main exposing (..)
 
 import Html exposing (Html, main_, header, h1, text)
+import Html.Attributes exposing (class)
+
 
 type alias Model =
     Int
+
 
 init : Model
 init =
     0
 
+
+
 -- Update
+
 
 type Msg
     = Increment
     | Decrement
 
+
 update : Msg -> Model -> Model
 update msg model =
     Debug.crash "TODO"
 
+
+
 -- View
+
 
 view : Model -> Html Msg
 view model =
     main_ []
-        [h1 [] [text "Hello World!"]]
+        [ h1 [] [ text "Counter" ]
+        , div [] ]
+
 
 main : Program Never Model Msg
 main =
     Html.beginnerProgram
         { model = init
         , view = view
-        , update = update }
+        , update = update
+        }
